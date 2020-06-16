6c8c6db0b2308f968a359c6cb5b1ef3170125bfb
Geza Kovacs <gkovacs@stanford.edu>
2020-06-16 00:37:29 -0700

    added install_requires

--- setup.py
+++ setup.py
@@ -5,14 +5,18 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="plotly-utils",
-    version="0.0.2",
+    version="0.0.3",
     author="Geza Kovacs",
     author_email="noreply@gkovacs.com",
     description="High level API for plotting via Plotly in Jupyter Notebook",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/gkovacs/python-plotly-utils",
+    url="https://github.com/gkovacs/plotly-utils",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'statsmodels>=0.10.1',
+        'plotly>=4.1.1',
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
